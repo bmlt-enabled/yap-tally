036f9b32d4e4dd586ac890fb0ca8bf00dedc94c0
Danny Gershman <danny.gershman@gmail.com>
2023-12-29 17:47:14 -0500

    changes for tally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+  let yapServers = [
+      {"name":"SEZF","url":"https://bmlt.sezf.org/zonal-yap/upgrade-advisor.php?override_service_body_config_id=43"},
+      {"name":"Archsearch","url":"https://archsearch.org/yap-4.3.0-beta1/upgrade-advisor.php"}
+  ]
+
+  const [yapServerData, setYapServerData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const responses = await Promise.all(yapServers.map(server => fetch(`https://corsproxy.io/?${encodeURIComponent(server.url)} `)));
+        const data = await Promise.all(responses.map(response => response.json()));
+
+        const combinedData = data.map((responseData, index) => ({
+            name: yapServers[index].name,
+            data: [responseData],
+        }));
+
+        setYapServerData(combinedData)
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h2>Yap Tally</h2>
+          <table border={1}>
+              <thead>
+              <tr>
+                  <th>Server</th>
+                  <th>Version</th>
+                  {/* Add more table headers if needed */}
+              </tr>
+              </thead>
+              <tbody>
+              {yapServerData.map(({ name, data }) => (
+                  data.map((serverData) => (
+                      <tr key={name}>
+                          <td>{name}</td>
+                          <td>{serverData.version}</td>
+                          {/* Add more table cells if needed */}
+                      </tr>
+                  ))
+              ))}
+              </tbody>
+          </table>
+      </div>
   );
 }
 
