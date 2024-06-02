9cd4e0e734121df302a6e70cb0fa2a90ab3a6746
Patrick Joyce <pjaudiomv@gmail.com>
2024-06-02 11:58:14 -0400

    mv to svelte
    rm it

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import YapTally from '$lib/YapTally.svelte';
+	import '../css/main.scss';
+</script>
+
+<svelte:head>
+	<link rel="manifest" href="manifest.json" />
+	<link rel="apple-touch-icon" href="apple-icon-180.png" />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="icon" href="favicon.png" />
+	<title>Yap Tally</title>
+</svelte:head>
+
+<YapTally />
